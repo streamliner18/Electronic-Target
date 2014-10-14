e3dddbb09e70216bd281475846399f2bd9f66617
StreamLiner <jimmy.xrail@me.com>
2014-10-14 16:21:27 +0800

    Initial structure

--- Electronic_Target/main.c
+++ Electronic_Target/main.c
@@ -3,13 +3,23 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+byte device_id;
+
 void initIO(void) {
-	
+    
+}
+
+void initWDT(void) {
+    
+}
+
+void initTimer(void) {
+    
 }
 
 int main(void) {
 	initIO();
-
+    
 	while (1) {
 	}
 	return 0; // never reached
